d6dd517a85d9fe36bca8f7bf7fd4fb6a66dae158
Mark Christiansen <hi@powellmark.com>
2023-08-28 11:33:45 +1000

    add middleware examples

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,43 @@
 import { Router } from "express";
 
+/* 
+router is a "sub-app". an Express app can multiple routers, 
+each with their own configs and rules.
+*/
+
 const router = Router();
+
+/* 
+* Products
+*/
+
+router.get("/product", (req, res) => {
+    res.status(200)
+    res.json({ message: "Products!" })
+})
+router.put("/product/:id", () => { })
+router.post("/product", () => { })
+router.delete("/product/:id", () => { })
+router.get("/product/:id", () => { })
+
+/* 
+* Updates
+*/
+
+router.get("/update", () => { })
+router.get("/update/:id", () => { })
+router.put("/update/:id", () => { })
+router.post("/update", () => { })
+router.delete("/update/:id", () => { })
+
+/* 
+* Update Point
+*/
+
+router.get("/updatepoint", () => { })
+router.get("/updatepoint/:id", () => { })
+router.put("/updatepoint/:id", () => { })
+router.post("/updatepoint", () => { })
+router.delete("/updatepoint/:id", () => { })
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
-import express from "express"
+import express from 'express'
+import router from './router'
+import morgan from 'morgan' // Monitoring library
+import cors from 'cors';
 
 const app = express()
 
+// Add middleware before anything else
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+/*
+    will get ?a=1,thing=someotherthing from google.com?a=1,thing=someotherthing
+    and put it into a object we can access like 'query.a', 'query.thing'
+*/
+
 app.get('/', (req, res) => {
-    console.log('Hello from express');
-    res.status(200)
     res.json({ message: "hello" })
 })
+
+app.use('/api', router)
 
 export default app;
